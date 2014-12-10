1a508825e49682b5c5988f999885e4ec9bb6166c
nownabe <nownabe@gmail.com>
2014-12-10 23:15:50 +0900

    FIX: container image name

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
-name_base="pxc"
+name_base="test_pxc"
 cluster_size=3
-image="nownabe/pxc:5.5"
+image="nownabe/test_pxc"
 
 hosts=()
 cluster_addr=""
@@ -12,17 +12,14 @@ for ((i=0; i<cluster_size; i++)); do
   echo "* exec: docker run -d --name=\"${name}\" ${image}"
   docker run -d --name="${name}" ${image}
 
-  echo "* exec: docker ps | grep "${name}" | awk '{print \$1}'"
   id=$(docker ps | grep "${name}" | awk '{print $1}')
   echo "ID of ${name}: ${id}"
 
-  echo "* exec: docker inspect --format=\"{{ .NetworkSettings.IPAddress }}\" ${id}"
   ip=$(docker inspect --format='{{ .NetworkSettings.IPAddress }}' ${id})
   echo "IP address of ${name}: ${ip}"
 
   echo "Waiting..."
-  echo "* exec: sleep 10"
-  sleep 10
+  sleep 3
 
   echo "Start MySQL service in ${name}"
   echo "* exec: ssh root@${ip} -i ./id_rsa -oStrictHostKeyChecking=no \"service mysql start --wsrep_cluster_address=gcomm://${cluster_addr}\""
@@ -38,4 +35,5 @@ for h in "${hosts[@]}"; do
   echo "* exec: ssh root@${h} -i ./id_rsa -oStrictHostKeyChecking=no \"sed -i 's|gcomm://|gcomm://${cluster_addr}|' /etc/my.cnf\""
   ssh root@${h} -i ./id_rsa -oStrictHostKeyChecking=no "sed -i 's|gcomm://|gcomm://${cluster_addr}|' /etc/my.cnf"
 done
+
 
